2bcc1f0be0d95fb6502f75e4ec3c7b891fab39c6
Matt Powley <mattpow35@gmail.com>
2016-09-16 08:53:16 -0600

    added if/else about name

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,10 +1,12 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
 	private MarshmallowMonster firstMonster;
+	private Scanner keyboardInput;
 	
 	public MonsterController()
 	{
@@ -16,20 +18,37 @@ public class MonsterController
 				                                    1,
 				                                    true,
 				                                    2);
+		keyboardInput = new Scanner(System.in);
 	}
 
 	public void start()
 	{
 		
 		System.out.println("Here is my monster: " + firstMonster);
-		System.out.println("My monster has " + firstMonster.geteyeCount() + " eye.");
-		if(firstMonster.geteyeCount() == 1)
-		{
-			System.out.println("HE ONLY HAS ONE EYE!! How Scary!!");
-		}
+		System.out.println("Me has " + firstMonster.geteyeCount() + " eye.");
+//		if (firstMonster.geteyeCount() == 1)
+//		{
+//			System.out.println("--HE ONLY HAS ONE EYE!! How Scary!!--");
+//		}
 		
-		System.out.println("He has " + firstMonster.getantennaCount() + " antennas, he uses them to feel around and sense his surroundings.");
+		System.out.println("Me has " + firstMonster.getantennaCount() + " antennas, me uses them to feel around and sense me surroundings.");
+		
+		System.out.println("Do you want to change me name?");
+		String answer = keyboardInput.nextLine();
+		
+		if (answer .equalsIgnoreCase("yes"))
+		{
+			System.out.println("What do you want my name to be?");
+			String newName = keyboardInput.nextLine();
+			firstMonster.setname(newName);
+		}
+		else
+		{
+			System.out.println("FINE, don't change my name! angry >:/");
+		}
+		System.out.println(firstMonster);
 	}
+
 	
 }
 
